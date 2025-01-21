5314572f077aac4a00d3e78b96c96020ecaa08da
gialongchuai <gialongchuai@gmail.com>
2025-01-21 17:18:12 +0700

    universe

---
+++ demo/src/main/java/com/example/demo/dto/request/LogoutRequest.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class IntroSpectRequest {
+    String token;
+}
