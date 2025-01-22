1285640a6d0e89237f3d55c1044534ec95f57aa4
gialongchuai <gialongchuai@gmail.com>
2025-01-22 20:44:23 +0700

    universe

--- demo/src/main/java/com/example/demo/configuaration/ApplicationInitConfig.java
+++ demo/src/main/java/com/example/demo/configuaration/ApplicationInitConfig.java
@@ -2,7 +2,6 @@ package com.example.demo.configuaration;
 
 import com.example.demo.entity.Role;
 import com.example.demo.entity.User;
-import com.example.demo.repository.RoleRepository;
 import com.example.demo.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -22,27 +21,24 @@ import java.util.Set;
 @Slf4j
 public class ApplicationInitConfig {
     PasswordEncoder passwordEncoder;
-    RoleRepository roleRepository;
-    UserRepository userRepository;
 
     // code mặc định run ứng nếu không thấy tài khoản admin thì mặc định tạo tài khoản (focus log)
     @Bean
-    public ApplicationRunner adminInitializer() {
+    ApplicationRunner applicationRunner(UserRepository userRepository) {
         return args -> {
-            String adminUsername = "admin";
-            String adminPassword = "admin123"; // Nên sử dụng mã hóa mật khẩu
-            Set<Role> adminRole = new HashSet<>();
-            adminRole.add(roleRepository.findByName("ADMIN"));
-
-            // Kiểm tra xem admin đã tồn tại chưa
-            if (userRepository.findByUsername(adminUsername).isEmpty()) {
-                User admin = User.builder()
-                        .username(adminUsername)
-                        .password(passwordEncoder.encode(adminPassword))
-                        .roles(adminRole)
+            if(userRepository.findByUsername("admin").isEmpty()) {
+                Set<Role> roles = new HashSet<>();
+                roles.add(Role.builder()
+                                .name(com.example.demo.enums.Role.ADMIN.name())
+                        .build());
+                User user = User.builder()
+                        .roles(roles)
+                        .username("admin")
+                        .password(passwordEncoder.encode("admin"))
                         .build();
-                log.info("Toi dang tao admin");
-                userRepository.save(admin);
+
+                userRepository.save(user);
+                log.warn("Admin has been created with default username & password: admin. Please change it!");
             }
         };
     }
